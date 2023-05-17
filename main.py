59579299d7b4ac67b9e98aecfd02ad9bde9a53a8
Hollis Colburn <hollismcolburn@gmail.com>
2023-05-17 15:43:38 -0700

    add virtual environment

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import files, compare
+
 if __name__ == "__main__":
-    pass
+
+    # TODO: using Mac order for testing, update to generic file name when finished
+    df_purchase_order, df_invoice = files.read_files('mac_po.csv', 'mac_invoice.csv')
+
+    print("purchase order columns")
+    print(df_purchase_order.columns)
+
+    print("invoice columns")
+    print(df_invoice.columns)
